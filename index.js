2da14ab3891ed4d7130d57a05968f6fd8e60690f
Oded Niv <oded.niv@gmail.com>
2018-06-10 10:14:11 +0300

    fix wrapper require in index, v1.0.1

--- index.js
+++ index.js
@@ -4,4 +4,4 @@
 const { configure, LOG_DEBUG } = require('cfn-custom-resource');
 if (process.env.DEBUG) configure({ logLevel: LOG_DEBUG });
 
-module.exports = require('./wrapper');
+module.exports = require('./lib/wrapper');
